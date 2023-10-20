0fa393f2c064fefc191443cf724a483c54fbfdbd
Rahmadi <147220595+Rahmadiilyas@users.noreply.github.com>
2023-10-20 20:27:54 +0800

    hari 12
    hari 12

---
+++ LatihanPerulangann.java
@@ -0,0 +1,25 @@
+package latihanperulangann;
+import java.util.Scanner;
+public class LatihanPerulangann {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int awal,akhir;
+        System.out.println("Bilangan ganjil");
+        System.out.print("Masukkan bilangan awal : ");
+        awal = input.nextInt();
+        System.out.print("Masukkan bilangan akhir : ");
+        akhir = input.nextInt();
+        
+        for (int a = awal; a <= akhir; a++){
+           int hasil = a%2;
+            if(hasil == 1){
+                System.out.print(a+" ");
+            }
+        
+    }
+     
+    }
+}
+    
+    
+
