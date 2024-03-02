bad6a6f6466800fa3314d124403319e8cc939f32
Rahmadi <147220595+Rahmadiilyas@users.noreply.github.com>
2024-03-02 14:29:52 +0800

    Create no5.java

---
+++ no5.java
@@ -0,0 +1,15 @@
+
+package tugaspbo;
+import java.util.Scanner;
+public class no5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan n : ");
+        int n = input.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("INFORMATIKA");
+            
+        }
+        
+    }
+}
